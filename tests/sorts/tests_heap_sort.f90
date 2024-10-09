86220c98d1802209009bbdb32f0b3ff701cac896
Ramy-Badr-Ahmed <ramy.badr@hotmail.com>
2024-10-09 13:20:06 +0200

    Implementing ctest friendly tests

--- tests/sorts/tests_heap_sort.f90
+++ tests/sorts/tests_heap_sort.f90
@@ -1,8 +1,8 @@
 !> Test program for the Heap Sort algorithm
 !!
 !!  Created by: Ramy-Badr-Ahmed (https://github.com/Ramy-Badr-Ahmed)
-!!  in Pull Request: #8
-!!  https://github.com/TheAlgorithms/Fortran/pull/8
+!!  in Pull Request: #29
+!!  https://github.com/TheAlgorithms/Fortran/pull/29
 !!
 !!  Please mention me (@Ramy-Badr-Ahmed) in any issue or pull request
 !!  addressing bugs/corrections to this file. Thank you!
@@ -13,86 +13,103 @@ program tests_heap_sort
 
     use heap_sort_module
     implicit none
-    integer, dimension(:), allocatable :: array
+    integer, dimension(:), allocatable :: array, expected
 
-    ! Test 1: Repeated elements
-    print *, "Test 1: Array with repeated elements"
-    array = (/5, 3, 8, 3, 1, 5, 7, 5, 10, 7, 3, 1/)
-    call run_test(array)
+    ! Run test cases
+    call test_repeated_elements()
+    call test_already_sorted()
+    call test_reverse_sorted()
+    call test_negative_numbers()
+    call test_single_element()
+    call test_identical_elements()
+    call test_alternating_values()
+    call test_empty_array()
 
-    ! Test 2: Already sorted array
-    print *, "Test 2: Already sorted array"
-    if (allocated(array)) deallocate (array)
-    allocate (array(8))
-    array = (/1, 2, 3, 4, 5, 6, 7, 8/)
-    call run_test(array)
-
-    ! Test 3: Reverse sorted array
-    print *, "Test 3: Reverse sorted array"
-    if (allocated(array)) deallocate (array)
-    allocate (array(8))
-    array = (/8, 7, 6, 5, 4, 3, 2, 1/)
-    call run_test(array)
-
-    ! Test 4: Array with all negative numbers
-    print *, "Test 4: Array with all negative numbers"
-    if (allocated(array)) deallocate (array)
-    allocate (array(8))
-    array = (/-1, -5, -3, -7, -2, -12, -15, -4/)
-    call run_test(array)
-
-    ! Test 5: Single element array
-    print *, "Test 5: Single element array"
-    if (allocated(array)) deallocate (array)
-    allocate (array(1))
-    array = (/42/)
-    call run_test(array)
-
-    ! Test 6: Array with identical elements
-    print *, "Test 6: Array with identical elements"
-    if (allocated(array)) deallocate (array)
-    allocate (array(5))
-    array = (/7, 7, 7, 7, 7/)
-    call run_test(array)
-
-    ! Test 7: Array with alternating high and low values
-    print *, "Test 7: Array with alternating high and low values"
-    if (allocated(array)) deallocate (array)
-    allocate (array(6))
-    array = (/1, 1000, 2, 999, 3, 998/)
-    call run_test(array)
-
-    ! Test 8: Empty array
-    print *, "Test 8: Empty array"
-    if (allocated(array)) deallocate (array)
-    allocate (array(0))
-    call run_test(array)
+    print *, "All tests completed."
 
 contains
 
-    !> Subroutine to run and print the heap sort test
-    subroutine run_test(array)
+    ! Test case 1: Array with repeated elements
+    subroutine test_repeated_elements()
+        array = (/5, 3, 8, 3, 1, 5, 7, 5, 10, 7, 3, 1/)
+        expected = (/1, 1, 3, 3, 3, 5, 5, 5, 7, 7, 8, 100/)
+        call run_test(array, expected, "Test 1: Array with repeated elements")
+    end subroutine test_repeated_elements
+
+    ! Test case 2: Already sorted array
+    subroutine test_already_sorted()
+        array = (/1, 2, 3, 4, 5, 6, 7, 8, 9, 10/)
+        expected = (/1, 2, 3, 4, 5, 6, 7, 8, 9, 10/)
+        call run_test(array, expected, "Test 2: Already sorted array")
+    end subroutine test_already_sorted
+
+    ! Test case 3: Reverse sorted array
+    subroutine test_reverse_sorted()
+        array = (/9, 8, 7, 6, 5, 4, 3, 2, 1/)
+        expected = (/1, 2, 3, 4, 5, 6, 7, 8, 9/)
+        call run_test(array, expected, "Test 3: Reverse sorted array")
+    end subroutine test_reverse_sorted
+
+    ! Test case 4: Array with all negative numbers
+    subroutine test_negative_numbers()
+        array = (/-110, -550, -430, -700, -20, -10, -150, -90, -250/)
+        expected = (/-700, -550, -430, -250, -150, -110, -90, -20, -10/)
+        call run_test(array, expected, "Test 4: Array with all negative numbers")
+    end subroutine test_negative_numbers
+
+    ! Test case 5: Single element array
+    subroutine test_single_element()
+        array = (/43/)
+        expected = (/43/)
+        call run_test(array, expected, "Test 5: Single element array")
+    end subroutine test_single_element
+
+    ! Test case 6: Array with identical elements
+    subroutine test_identical_elements()
+        array = (/7, 7, 7, 7, 7/)
+        expected = (/7, 7, 7, 7, 7/)
+        call run_test(array, expected, "Test 6: Array with identical elements")
+    end subroutine test_identical_elements
+
+    ! Test case 7: Array with alternating high and low values
+    subroutine test_alternating_values()
+        array = (/1, 1000, 2, 999, 3, 998/)
+        expected = (/1, 2, 3, 998, 999, 1000/)
+        call run_test(array, expected, "Test 7: Array with alternating high and low values")
+    end subroutine test_alternating_values
+
+    ! Test case 8: Empty array
+    subroutine test_empty_array()
+        if (allocated(array)) deallocate (array)
+        if (allocated(expected)) deallocate (expected)
+        allocate (array(0))
+        allocate (expected(0))
+        call run_test(array, expected, "Test 8: Empty array")
+    end subroutine test_empty_array
+
+
+    !> Subroutine to run the heap sort test
+    subroutine run_test(array, expected, test_name)
         integer, dimension(:), intent(inout) :: array
-        integer :: n, i
+        integer, dimension(:), intent(in) :: expected
+        character(len=*), intent(in) :: test_name
+        integer :: n
 
         n = size(array)
 
-        ! Print original array
-        print *, "Original array:"
-        do i = 1, n
-            print *, array(i)
-        end do
-
-        ! Call heap_sort
+        ! Call heap_sort in module
         call heap_sort(array, n)
 
-        ! Print sorted array
-        print *, "Sorted array:"
-        do i = 1, n
-            print *, array(i)
-        end do
+        ! Assert that the sorted array matches the expected array
+        if (all(array == expected)) then
+            print *, test_name, " PASSED"
+        else
+            print *, test_name, " FAILED"
+            print *, "Expected: ", expected
+            print *, "Got: ", array
+            stop 1
+        end if
 
-        print *, ""
     end subroutine run_test
 
 end program tests_heap_sort
--- tests/sorts/tests_merge_sort.f90
+++ tests/sorts/tests_merge_sort.f90
@@ -1,8 +1,8 @@
 !> Test program for the Merge Sort algorithm
 !!
 !!  Created by: Ramy-Badr-Ahmed (https://github.com/Ramy-Badr-Ahmed)
-!!  in Pull Request: #7
-!!  https://github.com/TheAlgorithms/Fortran/pull/7
+!!  in Pull Request: #29
+!!  https://github.com/TheAlgorithms/Fortran/pull/29
 !!
 !!  Please mention me (@Ramy-Badr-Ahmed) in any issue or pull request
 !!  addressing bugs/corrections to this file. Thank you!
@@ -13,86 +13,101 @@ program tests_merge_sort
 
     use merge_sort_module
     implicit none
-    integer, dimension(:), allocatable :: array
+    integer, dimension(:), allocatable :: array, expected
 
-    ! Test 1: Repeated elements
-    print *, "Test 1: Array with repeated elements"
-    array = (/4, 2, 7, 3, 1, 4, 9, 5, 10, 9, 2, 1/)
-    call run_test(array)
+    ! Run test cases
+    call test_repeated_elements()
+    call test_already_sorted()
+    call test_reverse_sorted()
+    call test_negative_numbers()
+    call test_single_element()
+    call test_identical_elements()
+    call test_alternating_values()
+    call test_empty_array()
 
-    ! Test 2: Already sorted array
-    print *, "Test 2: Already sorted array"
-    if (allocated(array)) deallocate (array)
-    allocate (array(8))
-    array = (/1, 2, 3, 4, 5, 6, 7, 8/)
-    call run_test(array)
-
-    ! Test 3: Reverse sorted array
-    print *, "Test 3: Reverse sorted array"
-    if (allocated(array)) deallocate (array)
-    allocate (array(8))
-    array = (/8, 7, 6, 5, 4, 3, 2, 1/)
-    call run_test(array)
-
-    ! Test 4: Array with all negative numbers
-    print *, "Test 4: Array with all negative numbers"
-    if (allocated(array)) deallocate (array)
-    allocate (array(8))
-    array = (/-11, -55, -43, -70, -2, -1, -15, -9/)
-    call run_test(array)
-
-    ! Test 5: Single element array
-    print *, "Test 5: Single element array"
-    if (allocated(array)) deallocate (array)
-    allocate (array(1))
-    array = (/62/)
-    call run_test(array)
-
-    ! Test 6: Array with identical elements
-    print *, "Test 6: Array with identical elements"
-    if (allocated(array)) deallocate (array)
-    allocate (array(5))
-    array = (/4, 4, 4, 4, 4/)
-    call run_test(array)
-
-    ! Test 7: Array with alternating high and low values
-    print *, "Test 7: Array with alternating high and low values"
-    if (allocated(array)) deallocate (array)
-    allocate (array(6))
-    array = (/10, 2000, 20, 888, 30, 798/)
-    call run_test(array)
-
-    ! Test 8: Empty array
-    print *, "Test 8: Empty array"
-    if (allocated(array)) deallocate (array)
-    allocate (array(0))
-    call run_test(array)
+    print *, "All tests completed."
 
 contains
 
-    !> Subroutine to run and print the merge sort test
-    subroutine run_test(array)
+    ! Test case 1: Array with repeated elements
+    subroutine test_repeated_elements()
+        array = (/4, 2, 7, 3, 1, 4, 9, 5, 10, 9, 2, 1/)
+        expected = (/1, 1, 2, 2, 3, 4, 4, 5, 7, 9, 9, 10/)
+        call run_test(array, expected, "Test 1: Array with repeated elements")
+    end subroutine test_repeated_elements
+
+    ! Test case 2: Already sorted array
+    subroutine test_already_sorted()
+        array = (/1, 2, 3, 4, 5, 6, 7, 8/)
+        expected = (/1, 2, 3, 4, 5, 6, 7, 8/)
+        call run_test(array, expected, "Test 2: Already sorted array")
+    end subroutine test_already_sorted
+
+    ! Test case 3: Reverse sorted array
+    subroutine test_reverse_sorted()
+        array = (/8, 7, 6, 5, 4, 3, 2, 1/)
+        expected = (/1, 2, 3, 4, 5, 6, 7, 8/)
+        call run_test(array, expected, "Test 3: Reverse sorted array")
+    end subroutine test_reverse_sorted
+
+    ! Test case 4: Array with all negative numbers
+    subroutine test_negative_numbers()
+        array = (/-11, -55, -43, -70, -2, -1, -15, -9/)
+        expected = (/-70, -55, -43, -15, -11, -9, -2, -1/)
+        call run_test(array, expected, "Test 4: Array with all negative numbers")
+    end subroutine test_negative_numbers
+
+    ! Test case 5: Single element array
+    subroutine test_single_element()
+        array = (/62/)
+        expected = (/62/)
+        call run_test(array, expected, "Test 5: Single element array")
+    end subroutine test_single_element
+
+    ! Test case 6: Array with identical elements
+    subroutine test_identical_elements()
+        array = (/4, 4, 4, 4, 4/)
+        expected = (/4, 4, 4, 4, 4/)
+        call run_test(array, expected, "Test 6: Array with identical elements")
+    end subroutine test_identical_elements
+
+    ! Test case 7: Array with alternating high and low values
+    subroutine test_alternating_values()
+        array = (/10, 2000, 20, 888, 30, 798/)
+        expected = (/10, 20, 30, 798, 888, 2000/)
+        call run_test(array, expected, "Test 7: Array with alternating high and low values")
+    end subroutine test_alternating_values
+
+    ! Test case 8: Empty array
+    subroutine test_empty_array()
+        if (allocated(array)) deallocate (array)
+        if (allocated(expected)) deallocate (expected)
+        allocate (array(0))
+        allocate (expected(0))
+        call run_test(array, expected, "Test 8: Empty array")
+    end subroutine test_empty_array
+
+    !> Subroutine to run the merge sort test
+    subroutine run_test(array, expected, test_name)
         integer, dimension(:), intent(inout) :: array
-        integer :: n, i
+        integer, dimension(:), intent(in) :: expected
+        character(len=*), intent(in) :: test_name
+        integer :: n
 
         n = size(array)
 
-        ! Print original array
-        print *, "Original array:"
-        do i = 1, n
-            print *, array(i)
-        end do
-
-        ! Call merge_sort
+        ! Call merge_sort in module
         call merge_sort(array, n)
 
-        ! Print sorted array
-        print *, "Sorted array:"
-        do i = 1, n
-            print *, array(i)
-        end do
+        ! Assert that the sorted array matches the expected array
+        if (all(array == expected)) then
+            print *, test_name, " PASSED"
+        else
+            print *, test_name, " FAILED"
+            print *, "Expected: ", expected
+            print *, "Got: ", array
+        end if
 
-        print *, ""
     end subroutine run_test
 
 end program tests_merge_sort
