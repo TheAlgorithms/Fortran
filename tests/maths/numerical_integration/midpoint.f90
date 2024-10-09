9b1ed6a28bc227dbe06011a66002f6c5fc2444b0
Ramy-Badr-Ahmed <ramy.badr@hotmail.com>
2024-10-09 21:29:01 +0200

    code_style fix

--- tests/maths/numerical_integration/midpoint.f90
+++ tests/maths/numerical_integration/midpoint.f90
@@ -31,7 +31,7 @@ contains
         lower_bound = 0.0_dp
         upper_bound = 1.0_dp
         panels_number = 1000000  ! Must be a positive integer
-        expected = 1.0_dp / 3.0_dp
+        expected = 1.0_dp/3.0_dp
         call midpoint(integral_result, lower_bound, upper_bound, panels_number, f_x_squared)
         call assert_test(integral_result, expected, "Test 1: ∫ x^2 dx from 0 to 1")
     end subroutine test_integral_x_squared_0_to_1
@@ -43,7 +43,7 @@ contains
         lower_bound = 0.0_dp
         upper_bound = 2.0_dp
         panels_number = 1000000  ! Must be a positive integer
-        expected = 8.0_dp / 3.0_dp
+        expected = 8.0_dp/3.0_dp
         call midpoint(integral_result, lower_bound, upper_bound, panels_number, f_x_squared)
         call assert_test(integral_result, expected, "Test 2: ∫ x^2 dx from 0 to 2")
     end subroutine test_integral_x_squared_0_to_2
@@ -52,7 +52,7 @@ contains
     subroutine test_integral_sin_0_to_pi()
         real(dp) :: lower_bound, upper_bound, integral_result, expected
         integer :: panels_number
-        real(dp), parameter :: pi = 4.D0 * DATAN(1.D0)  ! Define Pi. Ensure maximum precision available on any architecture.
+        real(dp), parameter :: pi = 4.D0*DATAN(1.D0)  ! Define Pi. Ensure maximum precision available on any architecture.
         lower_bound = 0.0_dp
         upper_bound = pi
         panels_number = 1000000  ! Must be a positive integer
@@ -91,7 +91,7 @@ contains
         real(dp), parameter :: pi = 4.D0*DATAN(1.D0)  ! Define Pi. Ensure maximum precision available on any architecture.
         integer :: panels_number
         lower_bound = 0.0_dp
-        upper_bound = pi / 2.0_dp
+        upper_bound = pi/2.0_dp
         panels_number = 1000000  ! Must be a positive integer
         expected = 1.0_dp
         call midpoint(integral_result, lower_bound, upper_bound, panels_number, cos_function)
@@ -137,7 +137,7 @@ contains
     ! Function for 1/x
     real(dp) function log_function(x)
         real(dp), intent(in) :: x
-        log_function = 1.0_dp / x
+        log_function = 1.0_dp/x
     end function log_function
 
     ! Function for cos(x)
